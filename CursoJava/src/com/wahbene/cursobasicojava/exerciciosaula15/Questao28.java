2062b26c3bb718b737eb02cd79eb255fbf3bf319
Wahbene H. <wahbene15@gmail.com>
2025-06-26 14:17:05 -0300

    Atualização - 26/06/25

--- CursoJava/src/com/wahbene/cursobasicojava/exerciciosaula15/Questao28.java
+++ CursoJava/src/com/wahbene/cursobasicojava/exerciciosaula15/Questao28.java
@@ -7,23 +7,22 @@ public class Questao28 {
         Scanner ler = new Scanner(System.in);
         String escolha, escolhacard;
         int quantidade;
-        double valor, desconto;
-        boolean cartao_tab;
+        double valor = 0, desconto = 0, totValor = 0;
+
 
 
         System.out.print("--- CASA DE CARNE DO TOTÓ --- " +
-                "Selecione qual carne escolherá\n" +
+                "\n\nSelecione qual carne escolherá:\n\n" +
                 "F - Filé Duplo\n" +
                 "A - Alcatra\n" +
-                "P - Picanha\n");
+                "P - Picanha\n\nSua opção: ");
         escolha = ler.next();
 
-        System.out.print("A compra será feita pelo Cartão Tabajara?");
+        System.out.println("A compra será feita pelo Cartão Tabajara? s/n");
         escolhacard = ler.next();
 
         //Desconto de 5%
         if (escolhacard.equalsIgnoreCase("S")) {
-            cartao_tab = true;
             if (escolha.equalsIgnoreCase("F")) {
 
                 System.out.print("Digite a quantidade que você quer comprar (em kg): ");
@@ -32,16 +31,18 @@ public class Questao28 {
                 if (quantidade <= 5) {
                     valor = quantidade * 4.90;
                     desconto = valor * 0.05;
+                    totValor = valor - desconto;
                 } else if (quantidade > 5) {
                     valor = quantidade * 5.80;
                     desconto = valor * 0.05;
+                    totValor = valor - desconto;
                 }
 
-                System.out.print("----- CUPOM FISCAL -----\n" +
+                System.out.print("\n----- CUPOM FISCAL -----\n" +
                         "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Filé Duplo\n" +
                         "Quantidade comprada: " + quantidade + "kg" +
-                        "Desconto: R$" + desconto + "\n" +
-                        "Valor total: R$" + valor + "\n" +
+                        "\nDesconto: R$" + desconto + "\n" +
+                        "Valor total: R$" + totValor + "\n" +
                         "Forma de Pagamento: Cartões Tabajara");
 
             } else if (escolha.equalsIgnoreCase("A")) {
@@ -52,16 +53,18 @@ public class Questao28 {
                 if (quantidade <= 5) {
                     valor = quantidade * 5.90;
                     desconto = valor * 0.05;
+                    totValor = valor - desconto;
                 } else if (quantidade > 5) {
                     valor = quantidade * 6.80;
                     desconto = valor * 0.05;
+                    totValor = valor - desconto;
                 }
 
-                System.out.print("----- CUPOM FISCAL -----\n" +
-                        "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Filé Duplo\n" +
+                System.out.print("\n----- CUPOM FISCAL -----\n" +
+                        "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Alcatra\n" +
                         "Quantidade comprada: " + quantidade + "kg" +
-                        "Desconto: R$" + desconto + "\n" +
-                        "Valor total: R$" + valor + "\n" +
+                        "\nDesconto: R$" + desconto + "\n" +
+                        "Valor total: R$" + totValor + "\n" +
                         "Forma de Pagamento: Cartões Tabajara");
 
             } else if (escolha.equalsIgnoreCase("P")) {
@@ -72,23 +75,24 @@ public class Questao28 {
                 if (quantidade <= 5) {
                     valor = quantidade * 6.90;
                     desconto = valor * 0.05;
+                    totValor = valor - desconto;
                 } else if (quantidade > 5) {
                     valor = quantidade * 7.80;
                     desconto = valor * 0.05;
+                    totValor = valor - desconto;
                 }
 
-                System.out.print("----- CUPOM FISCAL -----\n" +
-                        "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Filé Duplo\n" +
+                System.out.print("\n----- CUPOM FISCAL -----\n" +
+                        "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Picanha\n" +
                         "Quantidade comprada: " + quantidade + "kg" +
-                        "Desconto: R$" + desconto + "\n" +
-                        "Valor total: R$" + valor + "\n" +
+                        "\nDesconto: R$" + desconto + "\n" +
+                        "Valor total: R$" + totValor + "\n" +
                         "Forma de Pagamento: Cartões Tabajara");
 
             } else {
                 System.out.print("Opção Inválida");
             }
         } else {
-            cartao_tab = false;
             if (escolha.equalsIgnoreCase("F")) {
 
                 System.out.print("Digite a quantidade que você quer comprar (em kg): ");
@@ -100,12 +104,12 @@ public class Questao28 {
                     valor = quantidade * 5.80;
                 }
 
-                System.out.print("----- CUPOM FISCAL -----\n" +
+                System.out.print("\n----- CUPOM FISCAL -----\n" +
                         "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Filé Duplo\n" +
                         "Quantidade comprada: " + quantidade + "kg" +
-                        "Desconto: R$" + desconto + "\n" +
+                        "\nDesconto: R$" + desconto + "\n" +
                         "Valor total: R$" + valor + "\n" +
-                        "Forma de Pagamento: Dinheiro ou Cartão de Crédito");
+                        "Forma de Pagamento: Dinheiro ou Cartão");
 
             } else if (escolha.equalsIgnoreCase("A")) {
 
@@ -118,12 +122,12 @@ public class Questao28 {
                     valor = quantidade * 6.80;
                 }
 
-                System.out.print("----- CUPOM FISCAL -----\n" +
-                        "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Filé Duplo\n" +
+                System.out.print("\n----- CUPOM FISCAL -----\n" +
+                        "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Alcatra\n" +
                         "Quantidade comprada: " + quantidade + "kg" +
-                        "Desconto: R$" + desconto + "\n" +
+                        "\nDesconto: R$" + desconto + "\n" +
                         "Valor total: R$" + valor + "\n" +
-                        "Forma de Pagamento: Dinheiro ou Cartão de Crédito");
+                        "Forma de Pagamento: Dinheiro ou Cartão");
 
             } else if (escolha.equalsIgnoreCase("P")) {
 
@@ -136,12 +140,12 @@ public class Questao28 {
                     valor = quantidade * 7.80;
                 }
 
-                System.out.print("----- CUPOM FISCAL -----\n" +
-                        "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Filé Duplo\n" +
+                System.out.print("\n----- CUPOM FISCAL -----\n" +
+                        "Tipo de carne escolhida: " + escolha.toUpperCase() + " - Picanha\n" +
                         "Quantidade comprada: " + quantidade + "kg" +
-                        "Desconto: R$" + desconto + "\n" +
+                        "\nDesconto: R$" + desconto + "\n" +
                         "Valor total: R$" + valor + "\n" +
-                        "Forma de Pagamento: Dinheiro ou Cartão de Crédito");
+                        "Forma de Pagamento: Dinheiro ou Cartão");
 
             } else {
                 System.out.print("Opção Inválida");
