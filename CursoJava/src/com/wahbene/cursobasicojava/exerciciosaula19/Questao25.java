aa87d13fc111cd3f7a93b6af157c59d57cb01aa8
Wahbene H. <wahbene15@gmail.com>
2025-09-25 13:00:05 -0300

    Atualização - Vetores - 25/09/25

--- CursoJava/src/com/wahbene/cursobasicojava/exerciciosaula19/Questao25.java
+++ CursoJava/src/com/wahbene/cursobasicojava/exerciciosaula19/Questao25.java
@@ -5,10 +5,19 @@ import java.util.Random;
 public class Questao25 {
     public static void main(String[] args) {
         Random random = new Random(123);
-        int [] b = new int[3];
+        int[] a = new int[10], b = new int[10];
 
-        for (int i = 0; i < 3; i++) {
-            b[i] = random.nextInt();
+        for (int i = 0; i < 10; i++) {
+            a[i] = random.nextInt(10);
+            if (a[i] % 2 == 0) {
+                b[i] = 1;
+            } else {
+                b[i] = 0;
+            }
+
+            System.out.println(b[i]);
         }
+
+
     }
 }
