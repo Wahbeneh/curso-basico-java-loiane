3b6729be313056af067645ac5888a7709822ad4a
Wahbene H. <wahbene15@gmail.com>
2025-10-14 18:30:18 -0300

    Atualização - Vetores - 14/10/25

--- CursoJava/src/com/wahbene/cursobasicojava/exerciciosaula19/Questao42.java
+++ CursoJava/src/com/wahbene/cursobasicojava/exerciciosaula19/Questao42.java
@@ -6,7 +6,7 @@ public class Questao42 {
     public static void main(String[] args) {
         int[] a = new int[10];
         Random random = new Random();
-        int receber = 0;
+
 
         for (int i = 0; i < a.length; i++) {
             a[i] = random.nextInt(6);
